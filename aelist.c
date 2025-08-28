492657f0065474fe03e9fa5a5089f0db92c60bcf
lomaster <119946584+Lomasterrrr@users.noreply.github.com>
2025-08-28 13:43:40 +0300

    Update aelist.c

--- aelist.c
+++ aelist.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025, OldTeam
+ * Copyright (c) 2025, ktotonokto, AE-FOUNDATION
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
